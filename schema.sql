a7158d276b8584978cccda15595363c76a8c824d
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-09-14 10:47:52 +0100

    Add the quaries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+name varchar(100) NOT NULL,
+date_of_birth date NOT NULL,
+escape_attempts INT NOT NULL,
+neutered boolean NOT NULL,
+weight_kg decimal NOT NULL
 );
